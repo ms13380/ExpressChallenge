ec3a0ff796fc46ca8f678d47098682c2a0e82bc8
Michael Sullivan <104106828+ms13380@users.noreply.github.com>
2022-07-23 22:21:43 -0400

    Add files via upload

---
+++ serverport.js
@@ -0,0 +1,42 @@
+const PORT = process.env.PORT || 27060;
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+const allNotes = require('./db/db.json');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
